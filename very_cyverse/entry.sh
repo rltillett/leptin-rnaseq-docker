110b02e5b2f0e549dcd85aa55c3efcd989ec1f71
Kayla Bieser <5004142398@nsc.edu>
2020-07-16 11:29:29 -0700

    new very_cyverse build

---
+++ very_cyverse/entry.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+HOME=/home/gea_user
+
+mkdir -p $HOME/.irods
+
+#echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+
+#exec jupyter lab --no-browser --allow-root --ip=0.0.0.0
+
+jupyter \
+lab \
+--no-browser \
+--ip=0.0.0.0 \
+--NotebookApp.token='' \
+--NotebookApp.password='' \
+--notebook-dir='/home/gea_user' \
+--Session.username='gea_user'
